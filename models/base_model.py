55be61c258e39c14dec79f962a4acd607de11206
Moonwalkert3ch <6620@holbertsonstudents.com>
2023-10-09 14:06:46 -0500

    created instance of dictionary keyvalue represent

--- models/base_model.py
+++ models/base_model.py
@@ -7,12 +7,22 @@ from datetime import datetime
 class BaseModel:
     """Represents BaseModel class all others inherit from"""
 
-    def __init__(self):
-        """ Initializes a BaseModel"""
+    def __init__(self, *args, **kwargs):
+        """ Initializes a BaseModel
+        Param Args: Args - list of arguments
+        kwargs - dictionary of key:value arguments
+        """
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-
+        time = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, time)
+                else:
+                    self.__dict__[key] = value
     def __str__(self):
         """print/str representation of BaseModel instance"""
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
