18c9a20147c421ce363d1b0adcf31ab81ec46df8
Moonwalkert3ch <6620@holbertonstudents.com>
2023-10-09 16:44:06 -0700

    having issue with reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,7 +6,8 @@ from models.base_model import BaseModel
 
 
 class FileStorage:
-    """ serialize inst to a JSON file and deserialize JSON file to inst"""
+    """Represents storage engine"""
+
     __file_path = "file.json"
     __objects = {}
 
@@ -21,21 +22,18 @@ class FileStorage:
 
     def save(self):
         """serializes __objects ti json file"""
-        obdict = FileStorage.__objects
-        objdict = {obj: obdict[obj].to_dict() for obj in obdict.keys()}
         with open(FileStorage.__file_path, "w") as f:
-            json.dump(objdict, f)
+            data = {key: value.to_dict() for key, value in FileStorage.__objects.items()}
+            json.dump(data, f)
 
     def reload(self):
         """deserialize json file to __objects if it exists
         Raise exception: none
         """
-        try:
-            with open(FileStorage.__file_path) as f:
-                objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
-        except FileNotFoundError:
+        if not os.path.isfile(FileStorage.__file_path):
             return
+        with open(FileStorage.__file_path, "r") as f:
+            objdict = json.load(f)
+            objdict = {key: self.classes()[v["__class__"]](**value)
+                    for key, value in objdict.items()}
+            FileStorage.__objects = objdict
