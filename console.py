5a5dc91887aeee23b0b152d5b2efd9e8963fd24b
Moonwalkert3ch <6620@holbertonstudents.com>
2023-10-09 17:12:12 -0700

    basis of console interface

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""The entry point of the command interpreter"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """command interpreter class"""
+
+    prompt = "(hbnb)"
+
+    def do_quit(self, line):
+        """Exits"""
+
+        return True
+
+    def do_EOF(self, line):
+        """exits program"""
+
+        print()
+        return True
+
+    def emptyline(self):
+        """doesnt execute anything"""
+        pass
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
