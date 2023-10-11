7c97ef2e1567ae635275cc557e06205d14845d2c
Moonwalkert3ch <6620@holbertonstudents.com>
2023-10-10 17:05:17 -0700

    created city class and state

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define city class"""
+from models.base_model impot BaseModel
+
+
+class City(BaseModel):
+    """city inherits from BaseModel"""
+
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines State class"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """state inherited from basemodel"""
+
+    name = ""
